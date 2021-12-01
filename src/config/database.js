ab6a4dbab083f2c007623b552c86dd5599611042
Isaac Ramirez <iramirezc@live.com.mx>
2021-12-01 13:54:05 -0600

    Add Sequelize + DB configuration files.

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const config = {
+  username: process.env.MARIADB_USER || "root",
+  password: process.env.MARIADB_PASSWORD || null,
+  database: process.env.MARIADB_DATABASE || "my_db",
+  host: process.env.MARIADB_HOST || "127.0.0.1",
+  port: process.env.MARIADB_PORT || 3306,
+  dialect: "mariadb",
+};
+
+module.exports = {
+  development: { ...config },
+  test: { ...config },
+  production: { ...config },
+};
