d9a64960979572daa4abea3449d46dc27e7e6706
Isaac Ramirez <iramirezc@live.com.mx>
2021-12-17 03:15:33 -0600

    Silent logs with env var

--- src/middleware/logError.js
+++ src/middleware/logError.js
@@ -1,5 +1,8 @@
 module.exports = (err, req, res, next) => {
-  if (req.app.get("env") !== "production") {
+  if (
+    req.app.get("env") !== "production" &&
+    process.env.SILENT_LOGS !== "true"
+  ) {
     console.log("==================== Error ====================");
     console.error(err);
     console.log("================== End Error ==================");
