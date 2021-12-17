130699ca4dfb6901f928f7f7dcc7167df73fb179
Isaac Ramirez <iramirezc@live.com.mx>
2021-12-17 02:06:04 -0600

    Log errors for all envs except production.

--- src/middleware/logError.js
+++ src/middleware/logError.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-  if (req.app.get("env") === "development") {
+  if (req.app.get("env") !== "production") {
     console.log("==================== Error ====================");
     console.error(err);
     console.log("================== End Error ==================");
